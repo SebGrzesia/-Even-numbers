661647c4c0b2f9ad66c9a78b16d79066a6cb78f4
SebastianG <dicoud@gmail.com>
2023-02-15 14:36:09 +0100

    Done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter integrer number");
+            while (!int.TryParse(Console.ReadLine(), out n)){
+                Console.WriteLine("number is invalid");
+                Console.WriteLine("Enter valid integer number");
+            }
+
+            for (int i = 0; i <= n; i += 2 )
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
